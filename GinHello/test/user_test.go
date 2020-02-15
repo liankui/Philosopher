1f6b76b05fbc588eb831092212ff7a6fbcc38088
yueke <yueke@jiedaibao.com>
2020-02-15 13:23:06 +0800

    test(test): add TestUserSaveByQuery method :police_car_light:

--- GinHello/test/user_test.go
+++ GinHello/test/user_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yueekee/Philosopher/GinHello/initRouter"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -18,3 +19,13 @@ func TestUserSave(t *testing.T) {
 	assert.Equal(t, "用户:"+username+"已经保存", w.Body.String())
 }
 
+func TestUserSaveByQuery(t *testing.T) {
+	username := "eric"
+	age := 27
+	router := initRouter.SetupRouter()
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodGet, "/user?name="+username+"&age="+strconv.Itoa(age), nil)
+	router.ServeHTTP(w, request)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "用户:"+username+"年龄:"+strconv.Itoa(age)+"已经保存", w.Body.String())
+}
