cee8d0987b81e44a95af2a2510f03ce5a3861330
yueke <yueke@jiedaibao.com>
2020-02-17 11:33:46 +0800

    feat(model): add userModel save user

--- GinHello/model/userModel.go
+++ GinHello/model/userModel.go
@@ -1,7 +1,26 @@
 package model
 
+import (
+	"github.com/yueekee/Philosopher/GinHello/initDB"
+	"log"
+)
+
 type UserModel struct {
 	Email			string		`form:"email" binding:"email"`
 	Password		string		`form:"password"`
 	PasswordAgain	string		`form:"password-again" binding:"eqfield=Password"`
+}
+
+// 增加用户
+func (user *UserModel) save() int64 {
+	result, e := initDB.Db.Exec("insert into ginhello.user (email, password) values (?,?);",
+		user.Email, user.Password)
+	if e != nil {
+		log.Panicln("user insert err:", e.Error())
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		log.Panicln("user insert id err:", err.Error())
+	}
+	return id
 }
